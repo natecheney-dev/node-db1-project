19124d11925cee7147813a8fb0cae93f949a16dc
natecheney-dev <natecheney@gmail.com>
2021-12-06 14:17:06 -0600

    finished 1/3 middleware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,5 +1,41 @@
+const Account = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const { name, budget } = req.body
+  if(name === undefined || budget === undefined) {
+    next({
+      status: 400,
+      message: 'name and budget are required'
+    })
+
+  } else if(typeof name !== 'string') {
+    next({
+      status: 400,
+      message: 'name of account must be a string'
+    })
+
+  } else if(name.trim().length < 3 || name.trim().length > 100) {
+    next({
+      status: 400,
+      message: 'name of account must be between 3 and 100'
+    })
+
+  } else if(typeof budget !== 'number' || isNaN(budget)) {
+    next({
+      status: 400,
+      message: 'budget of account must be a number'
+    })
+
+  } else if(budget < 0 || budget > 1000000) {
+    next({
+      status: 400,
+      message: 'budget of account is too large or too small'
+    })
+    
+  } else {
+    req.body.name = req.body.name.trim()
+    next()
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
